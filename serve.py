329373be48470b7dd253b76ef34112b1ff54e62b
untitleio-cloud <326407522+untitleio-cloud@users.noreply.github.com>
2026-09-10 11:51:58 +0900

    Keep dev server script out of repo

--- serve.py
+++
@@ -1,8 +0,0 @@
-import http.server
-
-class NoCache(http.server.SimpleHTTPRequestHandler):
-    def end_headers(self):
-        self.send_header('Cache-Control', 'no-store')
-        super().end_headers()
-
-http.server.test(HandlerClass=NoCache, port=8123)
